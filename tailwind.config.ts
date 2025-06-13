9112a7df38f5f9c8afaaf4a313a3e070fc878d10
Jensfrank <everett7623@gmail.com>
2025-06-13 23:12:38 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',  
   ],
   theme: {
     container: {
