d1871468b7d8d9ec36017d6c246d7099c78ec51e
Jensfrank <everett7623@gmail.com>
2025-07-23 22:05:08 +0800

    Update text-case.ts

--- lib/tools/text-case.ts
+++ lib/tools/text-case.ts
@@ -1,3 +1,9 @@
+// 名称: 英文大小写转换ts
+// 描述: 快速转换英文文本的大小写格式，支持多种转换模式
+// 路径: seedtool/lib/tools/text-case.ts
+// 作者: Jensfrank
+// 更新时间: 2025-07-23
+
 export type TextCase = 'upper' | 'lower' | 'sentence' | 'title' | 'toggle';
 
 export function convertCase(text: string, textCase: TextCase): string {
