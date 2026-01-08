cb6a71953d723a1e1cfd08013711196e28db5090
Jensfrank <everett7623@gmail.com>
2026-01-08 13:50:52 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,9 @@
+// 名称: 全局工具函数
+// 描述: 提供类名合并(cn)等通用辅助函数，用于Tailwind CSS样式的动态处理
+// 路径: Globokit/lib/utils.ts
+// 作者: Jensfrank
+// 更新时间: 2026-01-08
+
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
