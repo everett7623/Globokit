a5e92b96618e58d04e33f8eb5917639e8cd543cd
Jensfrank <everett7623@gmail.com>
2026-01-08 13:29:07 +0800

    Update page.tsx

--- app/tools/holiday-query/page.tsx
+++ app/tools/holiday-query/page.tsx
@@ -1,8 +1,8 @@
 // 名称: 国际节假日查询
 // 描述: 查询全球主要贸易国家的节假日安排，便于外贸业务安排
-// 路径: seedtool/app/tools/holiday-query/page.tsx
+// 路径: Globokit/app/tools/holiday-query/page.tsx
 // 作者: Jensfrank
-// 更新时间: 2025-07-23
+// 更新时间: 2026-01-08
 
 'use client'
 
